49b784e5fc8db8df9901b746af46770f9a492f1b
beldaaaa <oldbanana@email.cz>
2024-05-13 19:31:53 +0200

    init commit

---
+++ src/main/java/springframework/springr2dbc/SpringR2DbcApplication.java
@@ -0,0 +1,13 @@
+package springframework.springr2dbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringR2DbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringR2DbcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/springframework/springr2dbc/SpringR2DbcApplicationTests.java
@@ -0,0 +1,13 @@
+package springframework.springr2dbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringR2DbcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
