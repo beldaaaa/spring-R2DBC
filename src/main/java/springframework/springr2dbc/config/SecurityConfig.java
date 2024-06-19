d7b2ff975a88bd2ae2092641b74ccbc037dfe6be
beldaaaa <oldbanana@email.cz>
2024-06-19 14:46:57 +0200

    SBA probes added

--- src/main/java/springframework/springr2dbc/config/SecurityConfig.java
+++ src/main/java/springframework/springr2dbc/config/SecurityConfig.java
@@ -2,16 +2,28 @@ package springframework.springr2dbc.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.boot.actuate.autoconfigure.security.reactive.EndpointRequest;
 
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
 
     @Bean
+    @Order(1)
+    SecurityWebFilterChain actuatorSecurityFilterChain(ServerHttpSecurity http) {
+        http.securityMatcher(EndpointRequest.toAnyEndpoint())
+                .authorizeExchange(authorize -> authorize.anyExchange().permitAll());
+
+        return http.build();
+    }
+
+    @Bean
+    @Order(2)
     SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
         http
                 .csrf((ServerHttpSecurity.CsrfSpec::disable))
